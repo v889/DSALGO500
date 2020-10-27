a83070583c7affbe53bfdcc8dadf5d942a91737e
v889 <72309826+v889@users.noreply.github.com>
2020-10-27 10:40:12 +0530

    Create minimum and maximum in array.c

---
+++ minimum and maximum in array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<malloc.h>
+
+int main()
+{
+    int n,i,j,max,min;
+    printf("enter lenght of array:");
+    scanf("%d",&n);
+    int *a=(int*)malloc(n*sizeof(int));
+    printf("element of array:\n");
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    min=a[1];
+    max=a[1];
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    printf("max %d\n",max);
+    printf("min :%d",min);
+}
